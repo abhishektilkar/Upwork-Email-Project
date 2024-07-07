296ab62b3f5e5127da33d615b291b7160fc327d1
Abhshek Tilkar <tiklarabhishek2000@gmail.com>
2024-07-07 21:58:57 +0530

    upworkgmail-project

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string
+
+}
